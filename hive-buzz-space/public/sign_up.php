432f9c68484ccb55020d6b580c63704f145a6ca0
AuniShah <radennn.auni@gmail.com>
2023-05-30 05:30:15 +0800

    👽Completed the all the beginner, intermediate, and advanced level page

--- hive-buzz-space/public/sign_up.php
+++ hive-buzz-space/public/sign_up.php
@@ -67,13 +67,13 @@
             }
             ?>
       <nav class="navbar navbar-expand-lg navbar-light sticky-top py-3 d-block" data-navbar-on-scroll="data-navbar-on-scroll">
-        <div style="margin-bottom:4%; height:10px;"class="container"><a class="navbar-brand" href="index.html"><img src="assets/img/gallery/HIVE-logo-updated-Guest.png"  height="70" alt="logo" /></a>
+        <div style="margin-bottom:4%; height:10px;"class="container"><a class="navbar-brand" href="index.php"><img src="assets/img/gallery/HIVE-logo-updated-Guest.png"  height="70" alt="logo" /></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"> </span></button>
           <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto pt-2 pt-lg-0 font-base">
-              <li class="nav-item px-2"><a class="nav-link active" aria-current="page" href="index.html">Home</a></li>
+              <li class="nav-item px-2"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
               <li class="nav-item px-2"><a class="nav-link" aria-current="page" href="contribute.html">Contribute</a></li>
-            </ul><a class="btn btn-primary order-1 order-lg-0" href="#!">Sign Up</a>
+            </ul><a class="btn btn-primary order-1 order-lg-0" href="sign_up.php">Sign Up</a>
             <form class="d-flex my-3 d-block d-lg-none">
               <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
               <button class="btn btn-outline-primary" type="submit">Search</button>
@@ -218,7 +218,7 @@
       <section class="bg-secondary">
         <div class="container">
             <div class="row">
-                <div class="col-12 col-sm-12 col-lg-6 mb-4 order-0 order-sm-0"><a class="text-decoration-none" href="index.html"><img src="assets/img/gallery/HIVE-logo_Tbg.png" height="150"/></a>
+                <div class="col-12 col-sm-12 col-lg-6 mb-4 order-0 order-sm-0"><a class="text-decoration-none" href="index.php"><img src="assets/img/gallery/HIVE-logo_Tbg.png" height="150"/></a>
                 <iframe style= "margin-right: 70%;" class= "text-light" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.1466274461595!2d101.7005614!3d3.0554056999999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc4abb795025d9%3A0x1c37182a714ba968!2sAsia%20Pacific%20University%20of%20Technology%20%26%20Innovation%20(APU)!5e0!3m2!1sen!2smy!4v1684836238184!5m2!1sen!2smy" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" alt="Asia Pacific University of Innovation & Technology"></iframe>
                 <p class="text-light"> <i class="fas fa-envelope me-3"> </i><a class="text-light" href="mailto:buzzbuzz.HIVE@gmail.com?subject=Hello&body=Good day%20HIVE,">buzzbuzz.HIVE@gmail.com</a></p>
                 <p class="text-light"> <i class="fas fa-phone-alt me-3"></i><a class="text-light" href="tel:1-800-800-2299">1-800-800-2299 <--Need support?</a></p>
@@ -229,7 +229,7 @@
                 <li class="lh-lg"><a class="text-200" href="about-us.html">How HIVE works</a></li>
                 <li class="lh-lg"><a class="text-200" href="contribute.html">Contribute</a></li>
                 <li class="lh-lg"><a class="text-200" href="help-center.html">HELP center</a></li>
-                <li class="lh-lg"><a class="text-200" href="#!">Sign in</a></li>
+                <li class="lh-lg"><a class="text-200" href="loginPage.php">Sign in</a></li>
             </ul>
         </div>
         <div style= "margin-top:10%;" class="col-6 col-sm-4 col-lg-2 mb-3 order-3 order-sm-2">
@@ -244,7 +244,7 @@
       <h5 class="lh-lg fw-bold text-light mb-4 font-sans-serif">Contact us</h5>
       <ul class="list-unstyled mb-md-4 mb-lg-0">
         <li class="lh-lg"><a class="text-200" href="mailto:buzzbuzz.HIVE@gmail.com?subject=Hello&body=Good day%20HIVE,">Email us</a></li>
-        <li class="lh-lg"><a class="text-200" href="#!">Join the HIVE</a></li>
+        <li class="lh-lg"><a class="text-200" href="sign_up.php">Join the HIVE</a></li>
       </ul>
     </div>
   </div>
